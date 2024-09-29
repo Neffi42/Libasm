769a6d4dbe6afdfc317fbddae99b822dc6ceb0b5
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-09-30 00:08:01 +0200

    ft_read done

---
+++ src/ft_read.s
@@ -0,0 +1,29 @@
+section .text
+	global ft_read
+	extern __errno_location
+
+ft_read:
+	test rsi, rsi
+	je .return_null
+	test rdx, rdx
+	je .return_null
+
+	mov rax, 0
+	syscall
+
+	cmp rax, 0
+	jl .on_error
+
+    ret
+
+.return_null:
+	mov rax, -1
+	ret
+
+.on_error:
+	neg		rax
+	mov		rdi, rax
+	call    __errno_location wrt ..plt
+	mov		[rax], rdi
+	mov		rax, -1
+	ret
